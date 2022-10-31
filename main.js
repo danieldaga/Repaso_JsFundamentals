382a63bb28061f48e780359b3983ba20fe83f3d2
danieldaga <daniel@arhrestauracion.com>
2022-10-31 21:17:27 +0100

    .

--- main.js
+++ main.js
@@ -24,6 +24,28 @@ function printArray(arr) {
 }
 console.log(printArray(arrOne));
 
-//exercise 3
+//exercise 3 ??
+
+let array1 = [1,0,2,3,4]
+let array2 = [3,5,6,7,8,13]
+function sumArray(array1, array2) {
+    for (let i = 0; i < array2.length; i++) {
+        console.log(array1[i] + array2[i]); 
+    }
+    return 
+}
+console.log(sumArray(array1, array2));
+
+//exercise 4
+
+// const array = ["apple", "orange", "banana", "mango", "watermelon", "grape", "peach"];
+
+// function element(arr,num) {
+//     for (let i = 0; i < arr.length; i++) {
+        
+//     }
+//     return
+// }
+// console.log(element(array, 2));
 
 
